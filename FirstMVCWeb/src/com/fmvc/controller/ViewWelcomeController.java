1c0c9213c2e33792f80c77186361d62dc8422732
pragathJdev <52105924+pragathJdev@users.noreply.github.com>
2019-06-22 20:06:05 +0530

    Add files via upload

---
+++ FirstMVCWeb/src/com/fmvc/controller/ViewWelcomeController.java
@@ -0,0 +1,18 @@
+package com.fmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class ViewWelcomeController extends AbstractController {
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		ModelAndView mav = null;
+		mav = new ModelAndView();
+		mav.setViewName("welcome");
+		return mav;
+	}
+}
